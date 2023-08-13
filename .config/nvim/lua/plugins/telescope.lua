4abf2efb4c739696bf8aacb1afd8eef6d5fcd42e
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2023-08-14 01:04:32 +0300

    [Nvim] Configure Telescope

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      { "<CR>", "<cmd>Telescope find_files<CR>", desc = "Telescope files" },
+      { "<Leader>l", "<cmd>Telescope buffers<CR>", desc = "Telescope buffers" },
+    },
+  },
+}
