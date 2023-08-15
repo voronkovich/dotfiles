82d3b776443e4d20d6e8212ad3622bdebcc43521
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2023-08-15 04:24:01 +0300

    [Nvim] Configure &wildignore option

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,4 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.path = "**"
+local o = vim.opt
+
+o.path = "**"
+o.wildignore = "*/vendor/*,*/node_modules/*,*/var/*,*/public/build/*"
