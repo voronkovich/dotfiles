5640aa5bdbe770ac5e2a6b58f1e9cb3f802f7ce0
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2023-08-15 07:32:11 +0300

    [Nvim] Use <C-a> for saving files

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -8,3 +8,6 @@ map("n", ";", ":")
 -- Move backward and forward in insert mode
 map("i", "<C-h>", "<C-o>h")
 map("i", "<C-l>", "<C-o>l")
+
+-- Save file
+map({ "i", "v", "n", "s" }, "<C-a>", "<cmd>w<cr><esc>", { desc = "Save file" })
