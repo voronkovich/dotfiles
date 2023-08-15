511fe5cf4755be7f93cd7d50e21023be009f4586
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2023-08-15 04:32:36 +0300

    [Nvim] Improve CS

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,8 +1,9 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+local autocmd = vim.api.nvim_create_autocmd
 
-vim.api.nvim_create_autocmd("FileType", {
+autocmd("FileType", {
   pattern = { "c", "cpp" },
   callback = function()
     vim.opt_local.path = "**,/usr/include/**"
