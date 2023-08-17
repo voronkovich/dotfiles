1e70d92d373f7208447c9f898e3fae72fdc5b954
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2023-08-18 01:03:42 +0300

    [Nvim] Configure code indentation

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,5 +3,13 @@
 -- Add any additional options here
 local o = vim.opt
 
+-- Open files with "gf"
 o.path = "**"
 o.wildignore = "*/vendor/*,*/node_modules/*,*/var/*,*/public/build/*"
+
+-- Code indentation
+o.tabstop = 4
+o.shiftwidth = 4
+o.smarttab = true
+o.expandtab = true
+o.smartindent = true
