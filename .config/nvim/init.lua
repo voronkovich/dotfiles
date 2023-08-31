c837462fdad3f99e1d1e6fd672d8635fcbf0a503
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2023-08-31 18:35:30 +0300

    [Nvim] Add snippet to debug LSP

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+if vim.env.LSP_DEBUG then
+  vim.lsp.set_log_level("debug")
+  if vim.fn.has("nvim-0.5.1") == 1 then
+    require("vim.lsp.log").set_format_func(vim.inspect)
+  end
+end
