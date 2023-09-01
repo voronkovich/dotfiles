0ad00b647f12fee4181e5cb8ac6292e11fd8d997
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2023-09-01 19:45:40 +0300

    [Nvim] Drop lsp-zero

---
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        phpactor = {},
+        tsserver = {},
+        clangd = {},
+        volar = {},
+      },
+    },
+  },
+}
