2208fb84ba80ccb225f61960caac0e948700e2ae
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2023-09-01 21:52:41 +0300

    [Nvim] Configure TailwindCSS

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -7,6 +7,18 @@ return {
         tsserver = {},
         clangd = {},
         volar = {},
+        tailwindcss = {
+          filetypes_exclude = { "markdown" },
+        },
+      },
+      setup = {
+        tailwindcss = function(_, opts)
+          local tw = require("lspconfig.server_configurations.tailwindcss")
+          --- @param ft string
+          opts.filetypes = vim.tbl_filter(function(ft)
+            return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+          end, tw.default_config.filetypes)
+        end,
       },
     },
   },
