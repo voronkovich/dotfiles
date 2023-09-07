da657fcb7092a16c42fdeabe8319f5fd64ff8f9e
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2023-09-07 20:12:24 +0300

    [Nvim] Configure luasnip

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -1,7 +1,45 @@
 return {
   {
     "L3MON4D3/LuaSnip",
-    config = function()
+    dependencies = {
+      {
+        "snippets/sniphpets-luasnip",
+        dev = true,
+        opts = {
+          strict_types = true,
+          final_classes = true,
+          common = {
+            enabled = true,
+          },
+          phpunit = {
+            enabled = true,
+          },
+          symfony = {
+            enabled = true,
+          },
+          doctrine = {
+            enabled = true,
+          },
+        },
+      },
+    },
+    keys = {
+      {
+        "<Tab>",
+        function()
+          return require("luasnip").expand_or_jumpable() and "<Plug>luasnip-expand-or-jump" or "<Tab>"
+        end,
+        expr = true,
+        silent = true,
+        mode = "i",
+      },
+    },
+    opts = {
+      store_selection_keys = "<Tab>",
+    },
+    config = function(_, opts)
+      require("luasnip").config.set_config(opts)
+
       require("luasnip.loaders.from_lua").lazy_load({
         paths = vim.fn.stdpath("config") .. "/snippets",
       })
