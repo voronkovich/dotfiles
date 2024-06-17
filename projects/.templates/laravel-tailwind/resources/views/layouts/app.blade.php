6fa96a9191eb867594c6a8558e7bb5286c3aed21
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2024-06-17 08:32:15 +0300

    [ZSH] Add recipes for Laravel and Laravel Tailwind

---
+++ projects/.templates/laravel-tailwind/resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title', config('app.name'))@hasSection('title') - {{ config('app.name') }}@endif</title>
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="h-full font-sans antialiased">
+        <div class="container mx-auto h-full px-4">
+            @yield('content')
+        </div>
+    </body>
+</html>
