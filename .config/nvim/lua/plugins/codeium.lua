5b5587c838a8c9b5bdfe5c8d1cba18e39608c9ba
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2024-07-10 04:39:07 +0300

    [Nvim] Update Codeium settings

--- .config/nvim/lua/plugins/codeium.lua
+++ .config/nvim/lua/plugins/codeium.lua
@@ -1,37 +1,12 @@
 return {
   {
-    "Exafunction/codeium.vim",
-    enabled = false,
-    event = "BufEnter",
+    "Exafunction/codeium.nvim",
     config = function()
-      vim.g.codeium_no_map_tab = true
-    end,
-    keys = {
-      {
-        "<C-g>",
-        function()
-          return vim.fn["codeium#Accept"]()
-        end,
-        mode = "i",
-        expr = true,
-        desc = "Codeium Accept",
-      },
-    },
-  },
-  {
-    "jcdickinson/codeium.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
-    },
-    config = function()
-      require("codeium").setup({})
-    end,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "codeium" })
+      require("codeium").setup({
+        enable_chat = true,
+        enable_local_search = true,
+        enable_index_service = true,
+      })
     end,
   },
 }
