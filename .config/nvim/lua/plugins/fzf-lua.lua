92e8482eae8d7011af193e497638dabd3f2f5f5a
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2025-02-19 21:22:42 +0300

    [NVim] Tweak fzf-lua

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -4,6 +4,7 @@ return {
     keys = {
       { "<CR>", "<cmd>FzfLua files<CR>", desc = "FzfLua files" },
       { "<Leader>l", "<cmd>FzfLua buffers<CR>", desc = "FzfLua buffers" },
+      { "<Leader>s", "<cmd>FzfLua lsp_document_symbols<CR>", desc = "FzfLua symbols" },
     },
   },
 }
