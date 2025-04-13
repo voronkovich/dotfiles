e3fb6e2d7f4ba3b02e439f9e2453fe99ac6383a0
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2025-04-13 03:10:13 +0300

    [NVim] Fix conform config

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -3,7 +3,7 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        javascript = { { "prettierd", "prettier" } },
+        javascript = { prettierd = true, prettier = true },
       },
     },
   },
