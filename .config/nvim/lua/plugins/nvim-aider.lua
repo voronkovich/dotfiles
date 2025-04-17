4124aa8b2198f85e5558d42200f0720fdcf80d41
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2025-04-17 03:33:45 +0300

    [Nvim] Update Aider keymaps

---
+++ .config/nvim/lua/plugins/nvim-aider.lua
@@ -0,0 +1,20 @@
+return {
+  "GeorgesAlkhouri/nvim-aider",
+  dependencies = {
+    "folke/snacks.nvim",
+  },
+  cmd = {
+    "AiderTerminalToggle",
+    "AiderHealth",
+  },
+  keys = {
+    { "<leader>a/", "<cmd>Aider toggle<cr>", desc = "Open Aider" },
+    { "<leader>as", "<cmd>Aider send<cr>", desc = "Send to Aider", mode = { "n", "v" } },
+    { "<leader>ac", "<cmd>Aider command<cr>", desc = "Send Command To Aider" },
+    { "<leader>ab", "<cmd>Aider buffer<cr>", desc = "Send Buffer To Aider" },
+    { "<leader>aa", "<cmd>Aider add<cr>", desc = "Add File to Aider" },
+    { "<leader>ad", "<cmd>Aider drop<cr>", desc = "Drop File from Aider" },
+    { "<leader>ar", "<cmd>Aider add readonly<cr>", desc = "Add File as Read-Only" },
+  },
+  config = true,
+}
