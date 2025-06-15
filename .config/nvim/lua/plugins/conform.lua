c2f81bd784a8a9c82985104352f650f11ce87306
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2025-06-16 02:26:15 +0300

    [Nvim] Configure conform to use pint

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -3,7 +3,7 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        php = { "php_cs_fixer" },
+        php = { "pint", "php_cs_fixer" },
       },
       formatters = {
         php_cs_fixer = {
