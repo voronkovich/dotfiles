12921a06c6b76d633d9465c8e6168c2d8422f867
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2025-07-28 08:47:39 +0300

    [zsh] Add template for Laravel recipe

---
+++ projects/.templates/laravel/resources/views/components/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ isset($title) ? $title.' - '.config('app.name') : config('app.name') }}</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="min-h-screen font-sans antialiased">
+    <div class="container mx-auto max-w-[1000px] px-4 py-8">
+        {{ $slot }}
+    </div>
+</body>
+</html>
