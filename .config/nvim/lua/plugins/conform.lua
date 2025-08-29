2628153464c5ee6afd38317536efffbf4973fd8f
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2025-08-30 02:27:33 +0300

    [NVim] Configure blade-formatter

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -4,6 +4,7 @@ return {
     opts = {
       formatters_by_ft = {
         php = { "pint", "php_cs_fixer", stop_after_first = true },
+        blade = { "blade-formatter", stop_after_first = true },
       },
       formatters = {
         php_cs_fixer = {
